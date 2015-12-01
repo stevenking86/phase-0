984c11ae6dfc765f50864ae7a3bd2226a678a695
Steven King <stevenking86@gmail.com>
2015-12-01 07:45:36 -0500

    complete reflection for 4.3

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -2,3 +2,37 @@ puts "What's your favorite number?"
 fave=gets.chomp
 better=fave.to_i + 1
 puts "Isn't " + better.to_s + " an even better number?"
+
+=begin
+
+How do you define a local variable?
+
+Well first of all I'm realzing the last time this question was posed I thought it was asking for a definition of a local variable. Now I'm reading it the way I think the question is intended. A local variable is defined with an equals sign. As in local_variable_name = something
+
+How do you define a method?
+
+  A method is defined using 'def' and then the method's name. For example: def great_method
+What is the difference between a local variable and a method?
+
+A variable makes an object. A method makes a method. An object is like the noun of ruby and a method is like the verb.
+
+How do you run a ruby program from the command line?
+
+type "ruby program_name"
+
+How do you run an RSpec file from the command line?
+
+type "rspec spec_file_name"
+What was confusing about this material? What made sense?
+
+I thought that this stuff was pretty straightforward. It's mostly review so nothing was too confusing. I'm getting used to the workflow we're using for these ruby exercises now which is a bit different than what we were doing previously. That may be the toughest adjustment so far.
+
+Links:
+
+https://github.com/stevenking86/phase-0/blob/master/week-4/define-method/my_solution.rb
+
+https://github.com/stevenking86/phase-0/blob/master/week-4/address/my_solution.rb
+
+https://github.com/stevenking86/phase-0/blob/master/week-4/math/my_solution.rb
+
+=end
